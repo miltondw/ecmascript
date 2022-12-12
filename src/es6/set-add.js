ceacde0e707b158c8f391a1a6433c1cd64b5cb30
miltondw <estradamilton2001@gmail.com>
2022-12-12 13:34:26 -0500

    sin repetir

--- src/es6/set-add.js
+++ src/es6/set-add.js
@@ -5,3 +5,8 @@ list.add("Item 1");
 list.add("Item 2").add("item 3");
 
 console.log(list);
+//Sin repetir
+const array = ["1a", "1a", "2b", "2b", "3c", "4d", "4d", "5e"];
+
+const sinRepetidos = [...new Set(array)];
+console.log(sinRepetidos);
