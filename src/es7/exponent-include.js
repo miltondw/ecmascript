2ffb729eb35ea4dc421deb974f8b39747bc36f7a
miltondw <estradamilton2001@gmail.com>
2022-12-12 14:27:02 -0500

    es7

---
+++ src/es7/exponent-include.js
@@ -0,0 +1,11 @@
+let base = 2;
+let exponent = 4;
+let result = base ** exponent;
+console.log(result);
+
+const includes = [1, 2, 3, 4, 5];
+const includesString = ["Pepe", "Luis", "Gabo", "Cheo"];
+console.log(includes.includes(3));
+console.log(includes.includes(6));
+console.log(includesString.includes("pepe"));
+console.log(includesString.includes("Pepe"));
