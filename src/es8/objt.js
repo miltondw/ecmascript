0548c0727e5e744e7939f1080951a14170999da7
miltondw <estradamilton2001@gmail.com>
2022-12-14 15:46:56 -0500

    es8 objt delete comment

--- src/es8/objt.js
+++ src/es8/objt.js
@@ -1,8 +1,8 @@
 //Entries
-// const countries = { MX: "Mexico", CO: "Colombia", CL: "Chile", PE: "Peru" };
-// const countriesArr = Object.entries(countries);
-// console.log(countriesArr);
-// const countriesObj=Object.fromEntries(countriesArr)
-// console.log(countriesObj);
-// console.log(Object.values(countriesArr));
-// console.log(Object.keys(countriesArr));
+const countries = { MX: "Mexico", CO: "Colombia", CL: "Chile", PE: "Peru" };
+const countriesArr = Object.entries(countries);
+console.log(countriesArr);
+const countriesObj = Object.fromEntries(countriesArr);
+console.log(countriesObj);
+console.log(Object.values(countriesArr));
+console.log(Object.keys(countriesArr));
