bffec67aff91d881c103a760fe229d0c710f168a
miltondw <estradamilton2001@gmail.com>
2022-12-14 15:59:06 -0500

    es10

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const regex = /\b(ipsum)+\b/g;
-const str =
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci alias reiciendis nihil facere reprehenderit laudantium inventore rem quisquam doloremque nobis ipsum perferendis, molestias ad quod beatae dolores iusto, eius eligendi ipsum?";
-
-for (let match of str.matchAll(regex)) {
-  console.log(Object.entries(match), "obj");
-  console.log(match, "match");
-}
